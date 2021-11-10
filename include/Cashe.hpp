47a4f9bcbc29cddc423b6f9767f711a0ec190184
dashasheveleva <photodoshfy@gmail.com>
2021-11-10 11:15:14 +0300

    v0.6

--- include/Cashe.hpp
+++ include/Cashe.hpp
@@ -18,12 +18,13 @@ private:
     string traverse;               // способ обхода
     vector <double> traverse_time; // кол-во экспериментов
     vector <size_t> bufsize;       // объём буфера
-    const vector <size_t> casheSize= { 640 * 1024,      // L1
+    vector <size_t> casheSize= { 640 * 1024,      // L1
                                        10*1024*1024,    // L2
                                        64*1024*1024 };  // L3
 
 public:
     Cashe();
+    ~Cashe();
     void print_experiments(const Cashe& c, ostream& os);
     void Cdirect();
     void Creverse();
